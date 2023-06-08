40dec2c5f337cfff0aa2c422a871af3ceb41f267
StevenUster <steveneliasuster@gmail.com>
2023-06-08 16:32:19 +0200

    Add predictor

--- generator/train.py
+++ generator/train.py
@@ -1,40 +1,103 @@
-from tensorflow import keras
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, LeakyReLU, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+
+def build_generator():
+    model = Sequential([
+        Dense(128, input_dim=100),  # 100 is the dimensionality of the input noise vector
+        LeakyReLU(0.01),
+        Dense(256),
+        LeakyReLU(0.01),
+        Dense(512),
+        LeakyReLU(0.01),
+        Dense(1024),
+        LeakyReLU(0.01),
+        Dense(784, activation='tanh')  # 784 is for 28x28 output image (if we consider it)
+    ])
+    return model
+
+def build_discriminator():
+    model = Sequential([
+        Dense(1024, input_dim=784),  # 784 is for 28x28 input image (if we consider it)
+        LeakyReLU(0.01),
+        Dense(512),
+        LeakyReLU(0.01),
+        Dense(256),
+        LeakyReLU(0.01),
+        Dense(128),
+        LeakyReLU(0.01),
+        Dense(1, activation='sigmoid')
+    ])
+    return model
+
+def build_gan(generator, discriminator):
+    model = Sequential()
+    model.add(generator)
+    model.add(discriminator)
+    return model
+
+# create the models
+generator = build_generator()
+discriminator = build_discriminator()
+
+# compile the discriminator
+discriminator.compile(loss='binary_crossentropy', optimizer=Adam())
+
+# make the discriminator untrainable when we are training the generator
+discriminator.trainable = False
+
+# create the GAN
+gan = build_gan(generator, discriminator)
+
+# compile the GAN
+gan.compile(loss='binary_crossentropy', optimizer=Adam())
+
 import numpy as np
 
-def prepare_data(sequence, n_steps_in, n_steps_out):
-    X, y = [], []
-    for i in range(len(sequence)):
-        end_ix = i + n_steps_in
-        out_end_ix = end_ix + n_steps_out
-        if out_end_ix > len(sequence):
-            break
-        seq_x, seq_y = sequence[i:end_ix], sequence[end_ix:out_end_ix]
-        X.append(seq_x)
-        y.append(seq_y)
-    return np.array(X), np.array(y)
+def train(generator, discriminator, gan, epochs=20000, batch_size=128, display_interval=1000):
+    # Generate a batch of noise inputs to feed to the GAN
+    input_noise = np.random.normal(0, 1, size=[batch_size, 100])
 
+    # Generate some labels
+    real_labels = np.ones((batch_size, 1))
+    fake_labels = np.zeros((batch_size, 1))
 
-n_steps_in, n_steps_out = 3, 2
-sequence = [i for i in range(100)]
+    for epoch in range(epochs):
+        
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+        
+        # Select a random batch of real images from your dataset here
+        # For this example we are just using random noise as our images
+        real_images = np.random.normal(0, 1, size=[batch_size, 784])
+        
+        # Generate a batch of new images
+        generated_images = generator.predict(input_noise)
+        
+        # Train the discriminator on real and fake images
+        discriminator_real_loss = discriminator.train_on_batch(real_images, real_labels)
+        discriminator_fake_loss = discriminator.train_on_batch(generated_images, fake_labels)
+        
+        # Calculate the total discriminator loss
+        discriminator_loss = 0.5 * np.add(discriminator_real_loss, discriminator_fake_loss)
+        
+        # ---------------------
+        #  Train Generator
+        # ---------------------
+        
+        # Generate a batch of noise inputs to feed to the GAN
+        input_noise = np.random.normal(0, 1, size=[batch_size, 100])
+        
+        # Train the GAN (with the discriminator weights frozen)
+        gan_loss = gan.train_on_batch(input_noise, real_labels)
 
-X, y = prepare_data(sequence, n_steps_in, n_steps_out)
+        # If at display interval, print the losses and save the model weights
+        if epoch % display_interval == 0:
+            print(f"Epoch {epoch}, Discriminator Loss: {discriminator_loss}, Generator Loss: {gan_loss}")
+            generator.save_weights('generator.h5')
+            discriminator.save_weights('discriminator.h5')
 
-X = X.reshape((X.shape[0], X.shape[1], 1))
-
-n_features = 1
-
-model = tf.keras.Sequential([
-    keras.layers.SimpleRNN(50, activation='relu', input_shape=(n_steps_in, n_features)),
-    keras.layers.Dense(n_steps_out)
-])
-
-model.compile(optimizer='adam', loss='mse')
-
-model.fit(X, y, epochs=200, verbose=0)
-
-x_input = np.array([100, 101, 102])
-x_input = x_input.reshape((1, n_steps_in, n_features))
-print(x_input)
-prediction = model.predict(x_input, verbose=0)
-print(prediction)
+# Call the training function
+train(generator, discriminator, gan)
